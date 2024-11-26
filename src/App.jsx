d71afbaf1ff375dcca08a0c5077d7d28aabc7fd7
vishalagasagi44 <45069863+vishalagasagi44@users.noreply.github.com>
2024-11-26 23:53:59 +0530

    add src folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
